0e9fb891782a1b45089e45c7651ae1f97730ca15
Shashank <shashankpateriya4@gmail.com>
2021-07-08 22:26:21 +0530

    [Shashank] Add: Calculating length of the line

--- LineComparison/Program.cs
+++ LineComparison/Program.cs
@@ -6,13 +6,15 @@ namespace LineComparisonProblem
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Line Comparison Computation.");
-            LineComparison line = new LineComparison();
-            double[] lineLength = new double[2];
-            for (int numOfLines = 0; numOfLines < 2; numOfLines++)
-            {
-                lineLength[numOfLines] = line.CalculateLength();
-            }
-            line.CompareLength(lineLength[0], lineLength[1]);
+            int x1, x2, y1, y2;
+            Console.WriteLine("Enter values x1 and y1");
+            x1 = Convert.ToInt32(Console.ReadLine());
+            y1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter values x2 and y2");
+            x2 = Convert.ToInt32(Console.ReadLine());
+            y2 = Convert.ToInt32(Console.ReadLine());
+            double length = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            Console.WriteLine("The length of the Line is : " + length);
         }
     }
 }
