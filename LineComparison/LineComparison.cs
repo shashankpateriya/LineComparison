8fa08b47f61034a0dbe6cabf7e7cd544e1cbe2ef
Shashank <shashankpateriya4@gmail.com>
2021-07-08 22:31:22 +0530

    [Shashank] Add: Comparing length of the lines using OOPS method

--- LineComparison/LineComparison.cs
+++ LineComparison/LineComparison.cs
@@ -26,11 +26,11 @@ namespace LineComparisonProblem
             }
             else if (diffInLength > 0)
             {
-                Console.WriteLine("Line1 is greater in length than Line2");
+                Console.WriteLine("Line1 is Greater in length than Line2");
             }
             else
             {
-                Console.WriteLine("Line1 is smaller in length than Line2");
+                Console.WriteLine("Line1 is Smaller in length than Line2");
             }
         }
     }
